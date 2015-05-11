47654a40285ab4f03448e174d38e6cdb3cbe2d16
Gopal Narayanan <gopal.narayan@gmail.com>
2015-05-10 20:10:27 -0400

    Added in a lot more rvalue reference code.

--- typededuce.cc
+++ typededuce.cc
@@ -9,6 +9,11 @@ class wrapped_vector
 	std::vector<int> m_intvec;
 	
 public:
+	wrapped_vector()
+	{
+		
+	}
+
 	wrapped_vector(const std::initializer_list<int> & il) : m_intvec(il)
 	{
 		
@@ -20,6 +25,39 @@ public:
 	}
 };
 
+// A trivial enum-class example.
+enum class TheStrictlyTypedEnum
+{
+	E_TSTE_0,
+	E_TSTE_1
+};
+
+enum class TheSecondStrictlyTypedEnum
+{
+	E_TSSTE_0,
+	E_TSSTE_1
+};
+
+// Some l/rvalue references. 
+// Not a reference.
+wrapped_vector v;
+// An lvalue reference, won't compile.
+#if 0
+wrapped_vector & lval_v = wrapped_vector();
+#endif
+// A const lvalue reference, will compile.
+const wrapped_vector & clval_v = wrapped_vector();
+// An rvalue reference to a temporary object, will compile.
+wrapped_vector && rval_v = wrapped_vector();
+// An rvalue reference to a object. Bjarne et al say it should compile
+// but it doesn't.
+// cf: https://www.artima.com/cppsource/rvalue.html
+#if 0
+wrapped_vector && crval_v = v;
+#endif
+
+// TODO: An illustration of move semantics using rvalues.
+// Move semantics are used to avoid unnecessary copying. 
 
 int main()
 {
@@ -34,6 +72,17 @@ int main()
 	wrapped_vector vc = {4, 5, 3, 3};
 	
 	std::cout << "vc size is " << vc.size() << std::endl;
+	
+#if 0 // Enable this for a very verbose compile error.	
+	if (TheStrictlyTypedEnum::E_TSTE_0 == TheSecondStrictlyTypedEnum::E_TSSTE_0)
+		std::cout << "This won't even get here, will it ?" << std::endl;
+#endif
+
+	// Uniform initializers.
+	std::vector<int> v_size8(8);
+	std::vector<int> v_size_1_unifinit{8};
+	std::cout << "size of v_size8 is " << v_size8.size() << std::endl;
+	std::cout << "size of v_size_1_unifinit is " << v_size_1_unifinit.size() << std::endl;
 	
 	return 0;
 }
